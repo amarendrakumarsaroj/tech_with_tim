13dfb1c8e1801a1316f80b412613f461d299039d
Amarendra Kumar <dominonepal@gmail.com>
2025-04-20 22:58:48 +0545

    Created the C of CRUD  API

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ from models import Contact
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
     contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json, contacts))
+    return jsonify({"contacts": json_contacts})
 
 
 
